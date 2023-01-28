ff9b68a663def2bd5eb3212ec8e8c9dea161fe7c
zhanyuhao <1633086394@qq.com>
2023-01-28 23:11:43 +0800

    Exception异常常见的问题

---
+++ src/main/java/com/zyh/interview/p1/Exception.java
@@ -0,0 +1,33 @@
+/**
+ * @author zhanyuhao
+ * @time 2023/1/28 21:36
+ */
+package com.zyh.interview.p1;
+
+import java.io.IOException;
+
+public class Exception {
+    public static void main(String[] args) {
+        try {
+            String str = null;
+            int len = str.length();
+
+            int result = 10 / 0; // 这行代码不会执行
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException");
+        }catch (ArithmeticException e) {
+            System.out.println("ArithmeticException");  // 不会执行
+        }finally {
+            System.out.println("exec finally block");
+        }
+        System.out.println("outside env");
+
+    }
+    public void testException(String fileName) throws IOException {
+        if(fileName == null){
+            throw new IOException();
+        }
+        int len = fileName.length();
+        System.out.println(len);
+    }
+}
