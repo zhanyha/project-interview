7d9e0284de27860a6c50e2f7685483679f861ae9
zhanyuhao <1633086394@qq.com>
2023-02-07 09:23:05 +0800

    静态代理和动态代理

---
+++ src/main/java/com/zyh/interview/java/proxy/JDKProxyTest.java
@@ -0,0 +1,12 @@
+/**
+ * @author zhanyuhao
+ * @time 2023/2/6 9:37
+ */
+package com.zyh.interview.java.proxy;
+
+public class JDKProxyTest {
+
+    public static void main(String[] args) {
+//        JdkProxyFactory
+    }
+}
