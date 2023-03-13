f19d5b6353edeefb652bfeebb5a843d13db3dce4
zhanyuhao <1633086394@qq.com>
2023-03-13 11:05:47 +0800

    解决查找表基本问题

---
+++ src/main/java/com/zyh/interview/algorithm/p2find/C1HappyNumber.java
@@ -0,0 +1,33 @@
+package com.zyh.interview.algorithm.p2find;
+
+import java.util.HashSet;
+
+/**
+ * @author zhanyh
+ * @time 2023/3/13 10:46
+ */
+public class C1HappyNumber {
+
+    public boolean isHappy(int n) {
+        String s = String.valueOf(n);
+        HashSet<String> set = new HashSet<>();
+        set.add(s);
+        while (true) {
+            int sum = 0;
+            for (int i = 0; i < s.length(); i++) {
+                sum += ( s.charAt(i) - '0' )*( s.charAt(i) - '0');
+            }
+            if(sum == 1)
+                return true;
+            s = String.valueOf(sum);
+            if(set.contains(s))
+                return false;
+            set.add(s);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new C1HappyNumber().isHappy(2));
+    }
+
+}
