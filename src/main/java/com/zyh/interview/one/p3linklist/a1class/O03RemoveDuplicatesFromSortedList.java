4ec5e4de3e34d59c6b18f5753a2a799d780c0363
zhanyuhao <1633086394@qq.com>
2023-07-13 15:34:55 +0800

    7/12 链表穿针引线

--- src/main/java/com/zyh/interview/one/p3linklist/a1class/O03RemoveDuplicatesFromSortedList.java
+++ src/main/java/com/zyh/interview/one/p3linklist/a1class/O03RemoveDuplicatesFromSortedList.java
@@ -1,10 +1,29 @@
 package com.zyh.interview.one.p3linklist.a1class;
 
+import com.zyh.interview.one.p3linklist.ListNode;
+
 /**
  * @description:
  * @author：zhanyh
  * @date: 2023/7/11
  */
-// 83
+// 83  https://leetcode.cn/problems/remove-duplicates-from-sorted-list/
+
 public class O03RemoveDuplicatesFromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;
+        ListNode pre = head, cur = pre.next;
+        while (cur != null) {
+            ListNode next = cur.next;
+            if (cur.val == pre.val) {
+                pre.next = cur.next;
+                cur = next;
+            } else {
+                pre = cur;
+                cur = next;
+            }
+
+        }
+        return head;
+    }
 }
