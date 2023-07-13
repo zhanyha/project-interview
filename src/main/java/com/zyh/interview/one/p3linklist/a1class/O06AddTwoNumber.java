53367eb8e6410221dc4c2dc711ebd42e54f3efc0
zhanyuhao <1633086394@qq.com>
2023-07-13 16:46:19 +0800

    7/13 链表相加问题

--- src/main/java/com/zyh/interview/one/p3linklist/a1class/O06AddTwoNumber.java
+++ src/main/java/com/zyh/interview/one/p3linklist/a1class/O06AddTwoNumber.java
@@ -1,9 +1,55 @@
 package com.zyh.interview.one.p3linklist.a1class;
 
+import com.zyh.interview.one.p3linklist.ListNode;
+import com.zyh.interview.one.p3linklist.ListUtils;
+
 /**
  * @description:
  * @author：zhanyh
  * @date: 2023/7/11
  */
+// 2  https://leetcode.cn/problems/add-two-numbers/
 public class O06AddTwoNumber {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int c = 0;
+        ListNode p1 = l1, p2 = l2;
+        ListNode head = new ListNode(-1);
+        ListNode cur = head;
+        while (p1 != null && p2 != null) {
+            int n = (p1.val + p2.val + c) % 10;
+            c = (p1.val + p2.val + c) / 10;
+            cur.next = new ListNode(n);
+            cur = cur.next;
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        while (p1 != null) {
+            int n = (p1.val + c) % 10;
+            c = (p1.val + p1.val + c) / 10;
+            cur.next = new ListNode(n);
+            cur = cur.next;
+            p1 = p1.next;
+        }
+
+        while (p2 != null) {
+            int n = (p2.val + c) % 10;
+            c = (p2.val + c) / 10;
+            cur.next = new ListNode(n);
+            cur = cur.next;
+            p2 = p2.next;
+        }
+        if(c != 0){
+            cur.next = new ListNode(c);
+        }
+
+        return head.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head1 = ListUtils.create(new int[]{8, 9, 9, 9, 9});
+        ListNode head2 = ListUtils.create(new int[]{8, 9, 9});
+        ListNode res = new O06AddTwoNumber().addTwoNumbers(head1, head2);
+        ListUtils.print(res);
+    }
 }
--- src/main/java/com/zyh/interview/one/p3linklist/a1class/O07AddTwoNumberII.java
+++ src/main/java/com/zyh/interview/one/p3linklist/a1class/O07AddTwoNumberII.java
@@ -1,9 +1,67 @@
 package com.zyh.interview.one.p3linklist.a1class;
 
+import com.zyh.interview.one.p3linklist.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * @description:
  * @author：zhanyh
  * @date: 2023/7/11
  */
+// 445 https://leetcode.cn/problems/add-two-numbers-ii/
 public class O07AddTwoNumberII {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(-1);
+        ListNode p1 = l1, p2 = l2, cur = null;
+
+        Stack<Integer> stk1 = new Stack<>();
+        Stack<Integer> stk2 = new Stack<>();
+        while (p1 != null) {
+            stk1.push(p1.val);
+            p1 = p1.next;
+        }
+        while (p2 != null) {
+            stk2.push(p2.val);
+            p2 = p2.next;
+        }
+        int c = 0;
+        while (!stk1.isEmpty() && !stk2.isEmpty()) {
+            Integer pop1 = stk1.pop();
+            Integer pop2 = stk2.pop();
+            ListNode node = new ListNode((pop1 + pop2 + c) % 10);
+            dummyHead.next = node;
+            node.next = cur;
+            cur = node;
+            c = (pop1 + pop2 + c) / 10;
+        }
+
+        while (!stk1.isEmpty()) {
+            Integer pop1 = stk1.pop();
+            ListNode node = new ListNode((pop1 + c) % 10);
+            dummyHead.next = node;
+            node.next = cur;
+            cur = node;
+            c = (pop1 + c) / 10;
+        }
+
+        while (!stk2.isEmpty()) {
+            Integer pop2 = stk2.pop();
+            ListNode node = new ListNode((pop2 + c) % 10);
+            dummyHead.next = node;
+            node.next = cur;
+            cur = node;
+            c = (pop2 + c) / 10;
+        }
+        if (c != 0) {
+            ListNode node = new ListNode( c );
+            dummyHead.next = node;
+            node.next = cur;
+            cur = node;
+        }
+
+        return dummyHead.next;
+    }
 }
